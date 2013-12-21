469aa50b2e4f95b1b9e1710ac12a08f172a3c3f2
Jeff Edwards <js_edwards@yahoo.com>
2013-12-21 06:24:44 -0800

    initialize rails 4 app with pg and rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ticketee::Application.config.secret_key_base = 'b86c42a32adcc93567a3aa67ad03574f1b011a7d8d02d76ee93db7dc42448c35f258ecfb1d495f662e1084a19d250f90643c141f8ba11b0067829989ef3a3519'
